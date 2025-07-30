d1d589b6d1570a89ae77721660b733dcc7ab42dc
Nate <142466177+NateCamChiao@users.noreply.github.com>
2025-07-30 13:47:57 -0700

    Adding comments and formatting

--- robot.py
+++ robot.py
@@ -13,17 +13,11 @@ os.environ["HALSIMXRP_PORT"] = "3540"
 class MyRobot(commands2.TimedCommandRobot):
     field: Field2d = Field2d()
     def __init__(self, period: float = wpilib.TimedRobot.kDefaultPeriod / 1000) -> None:
-        super().__init__(period)
         self.autonomousCommand = InstantCommand()
         
     def robotInit(self) -> None:
         super().robotInit()
         self.m_robotContainer: RobotContainer = RobotContainer()
-    # def disabledInit(self) -> None:
-    #     """This function is called once each time the robot enters Disabled mode."""
-
-    # def disabledPeriodic(self) -> None:
-    #     """This function is called periodically when disabled"""
 
     def autonomousInit(self) -> None:
         """This autonomous runs the autonomous command selected by your RobotContainer class."""
@@ -32,16 +26,25 @@ class MyRobot(commands2.TimedCommandRobot):
         if self.autonomousCommand:
             self.autonomousCommand.schedule()
 
-    # def autonomousPeriodic(self) -> None:
-    #     """This function is called periodically during autonomous"""
 
     def teleopInit(self) -> None:
-        # This makes sure that the autonomous command stops running when
-        # teleop starts running. If you want the autonomous to
-        # continue until interrupted by another command, remove
-        # this line or comment it out.
+        """
+        This makes sure that the autonomous command stops running when
+        teleop starts running. If you want the autonomous to
+        continue until interrupted by another command, remove
+        this line or comment it out.
+        """
         if self.autonomousCommand:
             self.autonomousCommand.cancel()
+
+    # def autonomousPeriodic(self) -> None:
+    #     """This function is called periodically during autonomous"""
+    
+    # def disabledInit(self) -> None:
+    #     """This function is called once each time the robot enters Disabled mode."""
+
+    # def disabledPeriodic(self) -> None:
+    #     """This function is called periodically when disabled"""
 
     # def teleopPeriodic(self) -> None:
     #     """This function is called periodically during operator control"""
