694752626286cef7299eed319906e8344783588d
Nate <142466177+NateCamChiao@users.noreply.github.com>
2025-07-30 13:48:06 -0700

    removing unused command

--- commands/forwardAuto.py
+++ commands/forwardAuto.py
@@ -2,6 +2,9 @@ from commands2 import Command
 import wpilib
 from subsystems.drivetrain import DriveTrain
 class DriveForward(Command):
+    """
+    Simple auto that will drive at 50% speed for 0.5 seconds
+    """
     def __init__(self, driveSub: DriveTrain) -> None:
         self.driveSub: DriveTrain = driveSub
         self.addRequirements(driveSub)
